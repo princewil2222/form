940eb0c817717f6e9f2b89f7647a9a57a3fb8d37
Sir Prince <princewilh@gmail.com>
2022-02-09 14:55:28 +0100

    changed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 const connectDB = async() =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
+        const conn = await mongoose.connect("mongodb+srv://princewilh:secured@2019@cluster0.1o4dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
